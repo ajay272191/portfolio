5f55db01ba5efdd4dcaa0a97ba2d4cb2539c0a0d
Ajay Kumar <ajayrmavs33236@gmail.com>
2022-06-17 23:09:53 +0530

    initial commit

---
+++ precache-manifest.b52f00cf2cc9068370861a8ab2819874.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789dd1ae8c9f85168a34ed8ad07a28d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d12c620c9d9362e89d19",
+    "url": "/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "64be9918b7fee2684d9b",
+    "url": "/static/css/main.cc7934f6.chunk.css"
+  },
+  {
+    "revision": "d12c620c9d9362e89d19",
+    "url": "/static/js/2.a5d97c67.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.a5d97c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64be9918b7fee2684d9b",
+    "url": "/static/js/main.6d0b1ecd.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "1e59b149a651faf69c886baf45d8c071",
+    "url": "/static/media/Child.1e59b149.png"
+  },
+  {
+    "revision": "6115d525bf6e0eca499678538f1740ac",
+    "url": "/static/media/Chip.6115d525.png"
+  },
+  {
+    "revision": "b9e3c20ea2e2320c9c51488b61e9f38c",
+    "url": "/static/media/Code.b9e3c20e.png"
+  },
+  {
+    "revision": "62f25141063cde81af312d9dfa5fcdd5",
+    "url": "/static/media/Eml.62f25141.png"
+  },
+  {
+    "revision": "38bdb4d4e2fdb8e6ae27a30c541abb60",
+    "url": "/static/media/Hardware.38bdb4d4.png"
+  },
+  {
+    "revision": "4f53444b67bcaee6c90f6bf672a428ac",
+    "url": "/static/media/Indoorpositioning.4f53444b.png"
+  },
+  {
+    "revision": "6330a089c3483afa07ee28daef44c461",
+    "url": "/static/media/Internship.6330a089.png"
+  },
+  {
+    "revision": "4cace44a7bed1b3909b6f38d463422bf",
+    "url": "/static/media/Lab.4cace44a.png"
+  },
+  {
+    "revision": "f490ecac149b5c233b46a096b38e6860",
+    "url": "/static/media/LocalizationAndMapping.f490ecac.png"
+  },
+  {
+    "revision": "ca2cc7014a018837957ff44888833d42",
+    "url": "/static/media/ManyInOneWearable.ca2cc701.png"
+  },
+  {
+    "revision": "cc341a79b7acffb1b0125620eb190705",
+    "url": "/static/media/PrintedCpwAntenna.cc341a79.png"
+  },
+  {
+    "revision": "ee607d07963a7ae243dce4a9c43b325f",
+    "url": "/static/media/PrintedElectronicsFor.ee607d07.png"
+  },
+  {
+    "revision": "cc341a79b7acffb1b0125620eb190705",
+    "url": "/static/media/PrintedMicrostripPatch.cc341a79.png"
+  },
+  {
+    "revision": "727b06cfaf1aa9bc61bd57decb8d8c30",
+    "url": "/static/media/Scholarship.727b06cf.png"
+  },
+  {
+    "revision": "948316dfd5d789803bab77e9a3b677e3",
+    "url": "/static/media/SoftwareAndTools.948316df.png"
+  },
+  {
+    "revision": "d51119140e26730f6909cceb16ed1346",
+    "url": "/static/media/StudentAfterGraduation.d5111914.png"
+  },
+  {
+    "revision": "1a05fb23ad80228df13f2884d76a18d6",
+    "url": "/static/media/StudentBeforeGraduation.1a05fb23.png"
+  },
+  {
+    "revision": "1d4a2f6e23985e7286d1d491f3990c69",
+    "url": "/static/media/TransparentOxides.1d4a2f6e.png"
+  },
+  {
+    "revision": "02b2178eab2d803100020cf9bf189a69",
+    "url": "/static/media/profilePic.02b2178e.jpg"
+  }
+]);
